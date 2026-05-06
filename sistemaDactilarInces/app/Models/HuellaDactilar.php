1134c8932291374fa453d2717792b86be2a22d68
Kend <kenderick2004@gmail.com>
2026-05-05 22:21:10 -0400

    back up antes de middlwares, huellas y funciones desechadas

---
+++ sistemaDactilarInces/app/Models/HuellaDactilar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HuellaDactilar extends Model
+{
+    protected $fillable = [
+        'huella_pulgar',
+        'huella_indice'
+    ];
+}
