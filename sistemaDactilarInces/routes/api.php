0e87c26e3bd793e0595ff1afdacf38a2e57c5126
Kend <kenderick2004@gmail.com>
2026-05-13 14:06:46 -0400

    rutas protegidas

--- sistemaDactilarInces/routes/api.php
+++ sistemaDactilarInces/routes/api.php
@@ -4,24 +4,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(base_path('routes/auth.php'));
 
-Route::prefix('cargo')->group(base_path('routes/cargo.php'));
-
-Route::prefix('feriado')->group(base_path('routes/feriado.php'));
-
-Route::prefix('rol')->group(base_path('routes/rol.php'));
-
-Route::prefix('privilegio')->group(base_path('routes/privilegio.php'));
-
-Route::prefix('role-privilegio')->group(base_path('routes/role-privilegio.php'));
-
-Route::prefix('empleado')->group(base_path('routes/empleado.php'));
-
-Route::prefix('asistencia')->group(base_path('routes/asistencia.php'));
-
-Route::prefix('horario')->group(base_path('routes/horario.php'));
-
-Route::prefix('inasistencia')->group(base_path('routes/inasistencia.php'));
-
 Route::middleware('jwt.auth')->group(function () {
-    // Other protected routes
+    Route::prefix('cargo')->group(base_path('routes/cargo.php'));
+
+    Route::prefix('feriado')->group(base_path('routes/feriado.php'));
+
+    Route::prefix('rol')->group(base_path('routes/rol.php'));
+
+    Route::prefix('privilegio')->group(base_path('routes/privilegio.php'));
+
+    Route::prefix('role-privilegio')->group(base_path('routes/role-privilegio.php'));
+
+    Route::prefix('empleado')->group(base_path('routes/empleado.php'));
+
+    Route::prefix('asistencia')->group(base_path('routes/asistencia.php'));
+
+    Route::prefix('horario')->group(base_path('routes/horario.php'));
+
+    Route::prefix('inasistencia')->group(base_path('routes/inasistencia.php'));
 });
